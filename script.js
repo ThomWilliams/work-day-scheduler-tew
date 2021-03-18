7c065393632727e77875245f9d433b8ec93e06cd
Thom Williams <thomwilliams1990@gmail.com>
2021-03-18 00:14:23 +0000

    HTML + CSS: Stylings added

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // save reference to important DOM elements
 var timeDisplayEl = $('#time-display');
+var workHourEl = $('#working-hour');
+var diaryEl = $('#diary-input');
+var saveEl = $('#save');
+
+// var input diary text 
+// funtion - on click + enter text
+
+// var save data - 
+// function - on click 
+
+// JSON data
+
+
+// function colour change for past, present, future 
+// Time boxes
+// https://stackoverflow.com/questions/62925657/can-anyone-help-me-colorcode-my-time-blocks-using-jquery
+
+
+
+
 
 // handle displaying the time - MOMENT
 function displayTime() {
@@ -7,6 +27,23 @@ function displayTime() {
     timeDisplayEl.text(rightNow);
 };
 
-
+// Displays Time
 setInterval(displayTime, 1000);
 
+
+/*
+insert timer
+create 3 column layout using bootstrap
+  -  define time ranges for workHourEl - create array of times and loop through?
+  -  create user text input for diaryEl
+  -  create saveEl Button
+Local storage / JSON function to save data after a refresh 
+define colour coding by past, present and future classes on the rows..... if (timeDisplay = rightnow)
+
+
+
+BOOT STRAP - lists - https://getbootstrap.com/docs/4.5/components/list-group/
+
+
+
+*/
